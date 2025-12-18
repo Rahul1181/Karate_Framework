c843b9d507fb832be735676260656268b4339b56
Rahul Mandal <rahulmandal99555@gmail.com>
2025-12-19 03:07:37 +0530

    first: Successful feature file output of Karate framework

--- karate_tutorial/src/test/java/runner/RunSampleOutputTest.java
+++ karate_tutorial/src/test/java/runner/RunSampleOutputTest.java
@@ -1,19 +1,11 @@
 package runner;
 
-// We need Runner class to run the feature file with more Options
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import com.intuit.karate.junit5.Karate;
 
-@RunWith(Cucumber.class)  
-@CucumberOptions(
-    features = "src/test/resources/features", //path of feature file
-    glue = {"stepDefinition"},
-    plugin = {"pretty", "html:target/HtmlReports/cucumber-report.html",
-                "json:target/JsonReports/cucumber-report.json",
-                "junit:target/JunitReports/cucumber-report.xml"},
-    monochrome = true  //makes console output more readable
-)
-public class RunSampleOutputTest {
-    
+class SampleOutputTest {
+
+    @Karate.Test
+    Karate testSampleOutput() {
+        return Karate.run("classpath:features/sampleOutput.feature");
+    }
 }
--- karate_tutorial/src/test/resources/features/sampleOutput.feature
+++ karate_tutorial/src/test/resources/features/sampleOutput.feature
@@ -3,3 +3,4 @@ Feature: We are covering up the basics of karate framework
 Scenario: This scenario covers print statement
 
     * print 'Welcome to karate tutorial project'
+    * karate.log("This checks the log output")
